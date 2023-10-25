5b6a0e0bb659190c561f49d4e26c4c8d7f99daec
mantillonv <marceloantillon93@gmail.com>
2023-10-24 17:55:57 -0700

    Add Color Select and Clear functions, fix New Grid

--- script.js
+++ script.js
@@ -1,138 +1,136 @@
+// Init //
 let penColor = "#000000";
-let fillColor = "#FFFFFF";
+let isDrawing = false;
 let eraserMode = false;
+let colorSelect = document.getElementById("color-select");
+colorSelect.addEventListener("input", changePenColor);
+document.getElementById("clear-button").addEventListener("click", function(event) {
+    event.preventDefault(); 
+    clearGrid(); 
+});
 
+// Panel //
 let containerDiv = document.querySelector(".container");
 let userValue = document.getElementById("user-number");
 let userSubmit = document.getElementById("user-submit")
 let promptText = document.getElementById("prompt");
 let copyInput = document.getElementById("copy-input");
-let clearButton = document.getElementById("clear-button");
+
+promptText.textContent = "";
 
 userValue.addEventListener("focus", entryHint);
-userValue.addEventListener("keyup", duplicateGrid);
-userValue.addEventListener("click", makeGrid);
-userValue.addEventListener("click", clearGrid);
+userValue.addEventListener("input", function(event) {
+    let number = parseInt(userValue.value);
+    if (number >= 2 && number <= 99 && !isNaN(number)) {
+        clearGrid();
+        clearHint();
+    } else {
+        promptText.textContent = "";
+    }
+});
 
-makeGrid();
+userSubmit.addEventListener("click", function(event) {
+    event.preventDefault();
+    duplicateGrid();
+});
+
 draw();
 
+let gridForm = document.getElementById("grid-form");
+
+gridForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    duplicateGrid();
+});
+
 function duplicateGrid() {
-    let userGrid = userValue.value;
+    let userGrid = parseInt(userValue.value);
     copyInput.textContent = "x " + userGrid;
+    makeGrid(userGrid);
 }
 
 function entryHint() {
     promptText.textContent = "Enter number between 2 and 99";
 }
 
-function makeGrid() {
-    let number = userValue.value;
-    if(number < 0 || number > 99 || isNaN(number)) {
-        promptText.textContent = "Oops! Not a valid number"
-    } else {
+function clearHint() {
     promptText.textContent = "";
-    copyInput.textContent = "";
-    userValue.value = "";
-    containerDiv.innerHTML = "";
-    if (number == 0 || number > 99 || number == "") {
-        for(let i = 0; i < 10; i++) {
+}
+
+function makeGrid() {
+    let number = parseInt(userValue.value);
+
+    if (number < 2 || number > 99 || isNaN(number)) {
+        promptText.textContent = "Oops! Not a valid number";
+    } else {
+        promptText.textContent = "";
+        copyInput.textContent = "";
+        userValue.value = "";
+        containerDiv.innerHTML = "";
+
+        for (let i = 0; i < number; i++) {
             let row = document.createElement("div");
             row.classList.add("row");
             containerDiv.appendChild(row);
-            for(let k = 0; k < 10; k++) {
-                let column = document.createElement("div");
-                column.classList.add("column")
-                row.appendChild(column);
-            }
-            }
-        }   else {
-        for(let i = 0; i < number; i++) {
-            let row = document.createElement("div");
-            row.classList.add("row");
-            containerDiv.appendChild(row);
-            for(let k = 0; k < number; k++) {
+
+            for (let k = 0; k < number; k++) {
                 let column = document.createElement("div");
                 column.classList.add("column");
+                column.style.backgroundColor = "#FFFFFF";
                 row.appendChild(column);
+            }
         }
+        draw();
     }
 }
-}
-draw();
-}
 
+// Controls //
 function draw() {
-    let columns = document.getElementsByClassName("column");
-    for(let i = 0; i < columns.length; i++) {
-        columns[i].addEventListener("mouseover", changeColor);
+    containerDiv.addEventListener("mousedown", startDrawing);
+    containerDiv.addEventListener("mouseup", stopDrawing);
+    containerDiv.addEventListener("mousemove", drawOnMove);
+    containerDiv.addEventListener("click", function(event) {
+    });
+}
+
+function startDrawing(event) {
+    if (event.target.classList.contains("column")) {
+        isDrawing = true;
+        changeColor(event);
     }
 }
 
-function changeColor() {
-    /* INSERT FUNCTION CODE HERE FOR ALL PALETTE BUTTONS */
+function stopDrawing() {
+    isDrawing = false;
 }
 
-function clearGrid() {
-    let columns = document.getElementByClassName("column");
-    for (let i = 0; i < columns.length; i++) {
-        columns[i].style.backgroundColor = "";
+function drawOnMove(event) {
+    if (isDrawing && event.target.classList.contains("column")) {
+        changeColor(event);
     }
-    }
+}
 
-    // Function to change pen color based on user input
 function changePenColor(event) {
     penColor = event.target.value;
 }
 
-// Function to change fill color based on user input
-function changeFillColor(event) {
-    fillColor = event.target.value;
-}
-
-// Function to toggle eraser mode
-function toggleEraserMode() {
-    eraserMode = !eraserMode;
-}
-
-// Function to handle mouseover events and change cell color
 function changeColor(event) {
     if (eraserMode) {
-        event.target.style.backgroundColor = ""; // Erase cell color
+        event.target.style.backgroundColor = "";
     } else {
-        event.target.style.backgroundColor = penColor; // Set pen color
+        event.target.style.backgroundColor = penColor;
     }
 }
 
-// Add event listeners for color palette buttons
-document.getElementById("color-select").addEventListener("input", changePenColor);
-document.getElementById("color-select-fill").addEventListener("input", changeFillColor);
-document.getElementById("eraser-btn").addEventListener("click", toggleEraserMode);
-document.getElementById("randomize-btn").addEventListener("click", randomizeColors);
-document.getElementById("clear-button").addEventListener("click", clearGrid);
-
-// Function to randomize colors of the grid
-function randomizeColors() {
-    let columns = document.getElementsByClassName("column");
-    for (let i = 0; i < columns.length; i++) {
-        columns[i].style.backgroundColor = getRandomColor();
-    }
-}
-
-// Function to generate a random color
-function getRandomColor() {
-    const letters = "0123456789ABCDEF";
-    let color = "#";
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
-
-// Function to clear the grid
 function clearGrid() {
-    let columns = document.getElementsByClassName("column");
-    for (let i = 0; i < columns.length; i++) {
-        columns[i].style.backgroundColor = "";
-    }
+    let columns = document.querySelectorAll(".column");
+    columns.forEach(column => {
+        column.style.backgroundColor = "#FFFFFF";
+    });
 }
+
+window.addEventListener("load", () => {
+    promptText.textContent = "";
+    makeGrid();
+});
+
